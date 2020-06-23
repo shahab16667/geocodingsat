e05a7683efef8f1469e0bcfb664df1e9c2e6c095
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-23 19:08:26 +1000

    map box working

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,35 +1,35 @@
 import mapboxgl from 'mapbox-gl';
 
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
+const mapElement = document.getElementById('map');
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
+const buildMap = () => {
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  return new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+  });
+};
 
-    const markers = JSON.parse(mapElement.dataset.markers);
+const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
-    })
+  });
+};
 
-    if (markers.length === 0) {
-      map.setZoom(1)
-    
-    } else if (markers.length === 1) {
-      map.setZoom(14);
-      map.setCenter ([markers[0].lng, makers[0].lat]);
-    } else {
-      const bounds = new mapboxgl.LngLatBounds();
-      markers.forEach((marker) => {
-        bounds.extend([maker.lng, maker.last]);
-      });
-      map.fitBounds(bounds, { duration: 10000, padding: 75})
-    }
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+};
+
+const initMapbox = () => {
+  if (mapElement) {
+    const map = buildMap();
+    const markers = JSON.parse(mapElement.dataset.markers);
+    addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
   }
 };
 
