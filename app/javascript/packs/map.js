be208f12b15e73decb7522b1bac56def2adc1f9c
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-23 19:00:14 +1000

    added map box

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -4,11 +4,32 @@ const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
   if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+    })
+
+    if (markers.length === 0) {
+      map.setZoom(1)
+    
+    } else if (markers.length === 1) {
+      map.setZoom(14);
+      map.setCenter ([markers[0].lng, makers[0].lat]);
+    } else {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach((marker) => {
+        bounds.extend([maker.lng, maker.last]);
+      });
+      map.fitBounds(bounds, { duration: 10000, padding: 75})
+    }
   }
 };
 
